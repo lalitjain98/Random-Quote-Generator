3ca30f7702730524c75fc0960a087105181577dd
Lalit Jain <jain.lalit138@gmail.com>
2019-01-12 15:43:29 +0530

    Updates

---
+++ precache-manifest.7c0fb730762a1aad28e641ca979282f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/random-quote-generator/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "d75e8004eb52303940f2",
+    "url": "/random-quote-generator/static/css/main.a827e8c3.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/random-quote-generator/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "632f6dd8ce581e02cd43",
+    "url": "/random-quote-generator/static/js/1.632f6dd8.chunk.js"
+  },
+  {
+    "revision": "7de484aaff74c1507f8a",
+    "url": "/random-quote-generator/static/js/runtime~main.7de484aa.js"
+  },
+  {
+    "revision": "d75e8004eb52303940f2",
+    "url": "/random-quote-generator/static/js/main.d75e8004.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/random-quote-generator/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/random-quote-generator/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/random-quote-generator/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "632f6dd8ce581e02cd43",
+    "url": "/random-quote-generator/static/css/1.918e7ce3.chunk.css"
+  },
+  {
+    "revision": "bc21751b3541c8b7cb757475e5075f1e",
+    "url": "/random-quote-generator/index.html"
+  }
+];
